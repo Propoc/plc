3fd7599dc4708e995b1e879a694b4a32ad8ee9df
Propoc <mert.ali.nemli@hotmail.com>
2025-12-27 23:35:02 +0300

    x22

--- ftp/ftp.js
+++ ftp/ftp.js
@@ -3,29 +3,30 @@ const fs = require('fs');
 
 const ftpServer = new FtpSrv({
   // Use 0.0.0.0 to listen on all interfaces
-  url: 'ftps://0.0.0.0:990', 
+  url: 'ftps://0.0.0.0:21', 
 
   pasv_url: '63.179.188.199', 
   pasv_min: 10000,
   pasv_max: 10005,
-  tls: {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-  }
+
+  anonymous: true 
 });
 
 ftpServer.on('client-connected', ({connection}) => {
   console.log('Client connected from:', connection.ip);
-  // You can send a custom message before they even try to log in
-  connection.reply(220, 'Anain amiha qoyayim');
+  connection.reply(220, 'Helloooo');
 });
 
-// Auto-resolve every login attempt (No actual password check)
+
 ftpServer.on('login', (data, resolve) => {
   console.log(`Login attempted by: ${data.username}`);
-  resolve({ root: './public' }); // Points to an empty folder
+  resolve({ root: './public' }); 
 });
 
 ftpServer.listen().then(() => {
-  console.log('FTPS Server is running on port 990');
+  console.log('FTPS Server is running on port 21');
+})
+
+ftpServer.on('error', (err) => {
+  console.error('Server Error:', err);
 });
