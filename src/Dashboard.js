2b099e2a63d31603d481ff026f0a12c34d6b87f2
Propoc <mert.ali.nemli@hotmail.com>
2025-12-30 15:08:10 +0300

    set1

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,67 +1,155 @@
 import React, { useEffect, useState } from "react";
 
-
-import Counter from "./Components/Counter";
-
-
 import { motion } from "framer-motion";
-import { Line as ChartLine } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend,Filler} from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
 
+// Adress farese write balonu unit "O" ise On Off
 const vars = {
   T1: { id: "T1", label: "Taze Hava Sıcaklık Bilgisi", unit: "°C" },
-  T2: { id: "T2", label: "Dönüş Hava SIcaklık Bilgisi", unit: "°C" },
+  T2: { id: "T2", label: "Dönüş Hava Sıcaklık Bilgisi", unit: "°C" },
   T3: { id: "T3", label: "Üfleme Hava Sıcaklık Bilgisi", unit: "°C" },
 
-  G1: { id: "G1", label: "Cihaz Durmu", unit: "O" },
+  G1: { id: "G1", label: "Cihaz Durmu", unit: "O" , addr: 16384 },
   G2: { id: "G2", label: "Sistem Durumu", unit: "O" },
   G3: { id: "G3", label: "Filtre Durumu", unit: "O" },
   G4: { id: "G4", label: "Genel Alarm", unit: "O" },
-  G5: { id: "G5", label: "Dönüş Sıcaklık Set", unit: "°C" },
-  G6: { id: "G6", label: "Vantilatör EC Fan Set", unit: "%" },
-  G7: { id: "G7", label: "Aspiratör EC Fan Set", unit: "%" },
-  G8: { id: "G8", label: "Üfleme Üst Limit", unit: "O" },
-  G9: { id: "G9", label: "Üfleme Alt Limit", unit: "O" },
+  G5: { id: "G5", label: "Dönüş Sıcaklık Set", unit: "°C", addr: 16387 },
+  G6: { id: "G6", label: "Vantilatör EC Fan Set", unit: "%" , addr: 16484 },
+  G7: { id: "G7", label: "Aspiratör EC Fan Set", unit: "%" , addr: 16483},
+  G8: { id: "G8", label: "Üfleme Alt Limit Set", unit: "°C" },
+  G9: { id: "G9", label: "Üfleme Alt Limit", unit: "°C" },
   G10: { id: "G10", label: "Üfleme Üst Limit Set", unit: "°C" },
-  G11: { id: "G11", label: "Üfleme Üst Limit Set", unit: "°C" },
+  G11: { id: "G11", label: "Üfleme Üst Limit", unit: "°C" },
+
+};
+
+const But = function But(
+  { isbool = true , result = 0,  addr = 0 , textsize = 'text-3xl' , handleWriteClick} 
+  ) {
+
+    const [hovered, setHovered] = useState(false);
+    const [text, setText] = useState("");
+    const [bubble, setBubble] = useState(false);
+
+    const handleBubble = () => {
+
+      if (isbool) {
+
+        if (result === -1) return;
+        let val = 1 - result;
+        handleWriteClick(addr,val);
+
+        return;
+      }
+
+
+      setBubble(!bubble);
+    };
+
+    const handleClick = () => {
+
+      handleWriteClick(addr,parseInt(text, 10));
+      return;
+
+    };
+
+    let display;
+    let clr = "bg-gray-400";
+
+    if (isbool)
+    {
+      if (result === -1) {
+        display = "Undefined";
+      }
+      else if (result === 0) {
+        display = "Kapalı";
+        clr = "bg-red-500";
+      }
+      else if (result === 1) {
+        display = "Açık";
+        clr = "bg-green-500";
+      }
+
+    }
+    else
+    {
+      display = result; 
+    }
+
+
+
+
+    return(
+      <div className="h-fit w-fit relative">
+        
+      <div
+        className={`
+          absolute bottom-full left-1/2 -translate-x-1/2
+          flex items-center gap-2
+          bg-white border rounded-xl px-2 py-2 shadow-lg z-10
+          transition-all duration-200 ease-out  
+          ${addr && !isbool && bubble
+            ? "opacity-100 scale-100 pointer-events-auto"
+            : "opacity-0 scale-95 pointer-events-none"}
+        `}
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="border px-3 py-2 rounded text-lg w-28"
+          placeholder="Değer"
+        />
+
+        <button
+          onClick={handleClick}
+          className="bg-green-500 text-white px-2 py-1 rounded-lg text-xl"
+        >
+          OK
+        </button>
+
+      </div>
+        
+        
+        <div className=" flex-[1] bg-blue-500">
+
+        </div>
+
+      <button
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={handleBubble}
+        className={`
+          w-fit h-fit rounded-full
+          flex items-center justify-center text-black 
+          transition-transform duration-150
+          ${textsize === 'text-3xl' ? "px-8 py-2" : "px-4 py-1" }
+          ${addr && hovered ? "scale-125 active:scale-90 bg-amber-300 " : "scale-100 " + clr }
+          ${addr ? "cursor-pointer " : "cursor-default " } 
+        `  +  textsize }
+      >
+
+        {display}
+
+      </button>
+    
+    
+     </div>
+    )
 
 };
 
 
 const Line = function Line(
-  {config = { label: "Placeholder" , unit:"V/V"} , type = 0 , history = []} 
-  ) {
+  { config = { label: "Placeholder", unit: "V/V" , addr : 0 }, type = 0, history = [] , textsize = 'text-3xl' , handleWriteClick }
+) {
 
-  let result = "0";
-
-  if (config.unit == "O"){
-    result = "Kapalı"
-  }
-  if (config.unit == "%"){
-    result = "0%"
-  }
 
   const sensorHistory = history[config.id] || [];
-  
-  const latestEntry = sensorHistory.at(-1); 
-  const latestValue = latestEntry ? latestEntry.val : 0;
+  const latestEntry = sensorHistory.at(-1);
+  const latestValue = latestEntry ? latestEntry.val : -1;
 
 
-  if (type === 3) {
-    return (
-      <div className={`w-full h-8 flex items-center justify-center `}>
-        <div className={`w-full h-full flex-[2] flex items-center justify-center text-black text-2xl `}>
-          {config.label}
-        </div>
-        <div className={`w-full h-ful flex-[1] flex items-center justify-center `}>
-          <button className= {`w-fit h-fit bg-gray-400 rounded-full px-8 flex items-center justify-center text-black text-2xl `} >  {result} </button> 
-        </div>         
-      </div>
-    );
-  }
 
   if (type === 2) {
     return (
@@ -70,7 +158,7 @@ const Line = function Line(
           {config.label}
         </div>
         <div className={`w-full h-full flex-[1] flex items-center justify-center `}>
-          <button className= {`w-fit h-fit bg-gray-400 rounded-full px-8 p flex items-center justify-center text-black text-3xl `} >  {result} </button> 
+          <button className= {`w-fit h-fit bg-gray-400 rounded-full px-8 p flex items-center justify-center text-black text-3xl `} >  {latestValue} </button> 
         </div>       
         <div className={`w-full h-full bg-slate-100 flex-[1] flex items-center justify-center text-black text-3xl `}>
           80%
@@ -86,15 +174,7 @@ const Line = function Line(
           {config.label}
         </div>      
         <div className={`w-full h-full  flex-[1] flex items-center justify-end pr-10 text-black text-3xl `}>
-          <Counter
-            value={latestValue}
-            places={[100, 10, 1]}
-            fontSize={40}
-            padding={0}
-            gap={0}
-            textColor="black"
-            fontWeight={"normal"}
-          />
+          {latestValue}
         </div>      
         <div className={`w-full h-full  flex-[1] flex items-center justify-start text-black text-3xl `}>
           {config.unit}
@@ -104,23 +184,33 @@ const Line = function Line(
     );
   }
 
+return(
+  <div className={`w-full h-[40px] flex items-center justify-center `}>
+    <div className={`w-full h-full flex-[2] flex items-center justify-center text-black ` + textsize}>
+      {config.label}
+    </div>
+    <div className={`w-full h-ful flex-[1] flex items-center justify-center `}>
 
-  return(
-      <div className={`w-full h-[40px] flex items-center justify-center `}>
-        <div className={`w-full h-full flex-[2] flex items-center justify-center text-black text-3xl `}>
-          {config.label}
-        </div>
-        <div className={`w-full h-ful flex-[1] flex items-center justify-center `}>
-          <button className= {`w-fit h-fit bg-gray-400 rounded-full px-8 py-2 flex items-center justify-center text-black text-3xl `} >  {result} </button> 
-        </div>         
-      </div>
+      <But isbool = {config.unit === "O"}  result = {latestValue} addr = {config.addr} textsize={textsize}  handleWriteClick = {handleWriteClick } />
+
+    </div>         
+  </div>
   );
+
+
+
+ 
 };
 
-const API_BASE =
-  process.env.REACT_APP_API_BASE ||
+
+
+
+
+const API_BASE =   
+  process.env.REACT_APP_API_BASE || "http://api.akscon.com" ||
   "http://localhost:4000";
 
+  
 
 const historyLen = 15;
 
@@ -132,20 +222,23 @@ const statusColors = {
 
 
 export default function Dashboard() {
-  const [history, setHistory] = useState({ T1: [], T2: [], T3: [] });
+
+  const initialHistory = Object.keys(vars).reduce((acc, key) => {
+    acc[key] = [];
+    return acc;
+  }, {});
+
+  const [history, setHistory] = useState(initialHistory);
+
   const [status, setStatus] = useState("Connection not started");
-
-
   const [topics, setTopics] = useState("tmsig-1/data");
   const [eventSource, setEventSource] = useState(null);
 
 
-  const [addrInput, setAddrInput] = useState("");
-  const [valInput, setValInput] = useState("");
   const [writePreview, setWritePreview] = useState("");
   const [writeStatus, setWriteStatus] = useState("");
 
-
+  const [flashBlue, setFlashBlue] = useState(false);
   
 
   useEffect(() => {
@@ -160,6 +253,12 @@ export default function Dashboard() {
     es.onerror = () => setStatus("Error");
 
     es.onmessage = (event) => {
+
+      setFlashBlue(true);
+      setTimeout(() => {
+        setFlashBlue(false);
+      }, 500);
+
 
       const msg = JSON.parse(event.data);
       const json = msg.data;
@@ -180,45 +279,51 @@ export default function Dashboard() {
           };
 
           next[key] = [...currentHistory, newDataPoint].slice(-historyLen);
-          console.log(`✅ ${key} updated. Points:`, next[key].length);
         });
         
         return next;
       });
+
+              
+
     };
+
 
     setEventSource(es);
     return () => es.close();
  
-  }, [topics]);
+  }, [topics,history]);
 
 
-  const handleWriteClick = async () => {
-    if (!addrInput || !valInput) return;
+  const handleWriteClick = async (addr,val) => {
+
+    if (writeStatus === "Sending…") {console.log("Send in progress wait"); return; }
 
     setWriteStatus("Sending…");
-
+    
     try {
       const res = await fetch(`${API_BASE}/write`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          address: addrInput.trim(),
-          value: valInput.trim(),
+          address: addr.trim(),
+          value: val.trim(),
         }),
       });
-
       const json = await res.json();
 
       if (!res.ok) {
         setWriteStatus("❌ Server rejected write");
+        console.log("Write - Server reject");
         return;
       }
-
       setWritePreview(json.content);
       setWriteStatus("✅ Written to test.txt");
+      console.log("Write - Success");
     } catch {
       setWriteStatus("❌ Network error");
+      console.log("Write - Network error");
+      
     }
   };
 
@@ -226,10 +331,17 @@ export default function Dashboard() {
   const c1 = "bg-[#3CCE58]";
   const c2 = "bg-slate-100";
 
+  const dotColor = flashBlue
+  ? "#3b82f6" // blue-500
+  : statusColors[status]?.dot || "#94a3b8";
+
+const shadowColor = flashBlue
+  ? "rgba(59,130,246,0.8)"
+  : statusColors[status]?.shadow || "rgba(0,0,0,0)";
 
   return (
     <div className={`w-full h-screen ${c2}`}>
-
+      
     {/* Top Bar */}
     <div
       className={`w-full h-32 bg-gradient-to-r from-green-300 via-[#3CCE58] to-green-300 flex justify-center items-center`}
@@ -243,39 +355,40 @@ export default function Dashboard() {
         AKSCON OTOMASYON KONTROL SİSTEMLERİ
       </div>   
 
-      <div className="w-full h-full flex-[1] flex justify-center items-center ">
-          {/* Outer Pulsing Glow */}
-          <motion.div
-            animate={{
-              scale: [1, 1.25, 1],
-              opacity: [0.4, 0.3, 0.4],
-            }}
-            transition={{
-              duration: 2,
-              repeat: Infinity,
-              ease: "easeInOut",
-            }}
-            className="absolute w-24 h-24 rounded-full blur-xl"
-            style={{
-              backgroundColor: statusColors[status]?.dot || "#94a3b8",
-            }}
-          />
-          {/* Inner Solid Bubble */}
-          <motion.div
-            animate={{
-              scale: [1, 1.05, 1],
-            }}
-            transition={{
-              duration: 2,
-              repeat: Infinity,
-              ease: "easeInOut",
-            }}
-            className="relative w-20 h-20 rounded-full shadow-2xl border-4 border-white/40"
-            style={{
-              backgroundColor: statusColors[status]?.dot || "#94a3b8",
-              boxShadow: `0 0 40px ${statusColors[status]?.shadow || "rgba(0,0,0,0)"}`,
-            }}
-          />
+      <div className="w-full h-full flex-[1] flex justify-center items-center">
+        {/* Outer Pulsing Glow */}
+        <motion.div
+          animate={{
+            scale: [1, 1.25, 1],
+            opacity: [0.4, 0.3, 0.4],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="absolute w-24 h-24 rounded-full blur-xl"
+          style={{
+            backgroundColor: dotColor,
+          }}
+        />
+
+        {/* Inner Solid Bubble */}
+        <motion.div
+          animate={{
+            scale: [1, 1.05, 1],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="relative w-20 h-20 rounded-full shadow-2xl border-4 border-white/40"
+          style={{
+            backgroundColor: dotColor,
+            boxShadow: `0 0 40px ${shadowColor}`,
+          }}
+        />
       </div>
 
     </div>
@@ -315,10 +428,10 @@ export default function Dashboard() {
       {/* Left */}
       <div className={`w-full h-full ${c2} flex-[2] flex flex-col items-center justify-evenly text-black text-3xl `}>
         
-        <Line config={vars.G1}/>
-        <Line config={vars.G2}/>
-        <Line config={vars.G3}/>
-        <Line config={vars.G4}/>
+        <Line config={vars.G1} history={history} handleWriteClick={handleWriteClick}/>
+        <Line config={vars.G2} history={history}/>
+        <Line config={vars.G3} history={history}/>
+        <Line config={vars.G4} history={history}/>
 
       </div>
       
@@ -327,7 +440,7 @@ export default function Dashboard() {
         
         <div  className={`w-full h-full bg-slate-100 flex-[1] border-black border-l-2 border-r-2  flex flex-col items-center justify-center text-black text-3xl `}>
 
-          <Line config={vars.G5}/>
+          <Line config={vars.G5} history={history} handleWriteClick={handleWriteClick}/>
 
         </div>
 
@@ -352,8 +465,8 @@ export default function Dashboard() {
         {/* Right  Top*/}
         <div className={`w-full h-full bg-slate-100 flex-[1] flex flex-col items-center justify-evenly`}>
     
-          <Line config={vars.G6} type = {2} />
-          <Line config={vars.G7} type = {2} />
+          <Line config={vars.G6} history={history} type = {2} />
+          <Line config={vars.G7} history={history} type = {2} />
 
 
         </div>
@@ -361,10 +474,10 @@ export default function Dashboard() {
         {/* Right  Bottom*/}
         <div className={`w-full h-full bg-slate-100 flex-[2] flex flex-col items-center justify-evenly text-black text-3xl `}>
           
-          <Line config={vars.G8} type = {3}/>
-          <Line config={vars.G9} type = {3}/>
-          <Line config={vars.G10} type = {3}/>
-          <Line config={vars.G11} type = {3}/>
+          <Line config={vars.G8} history={history} textsize="text-2xl"/>
+          <Line config={vars.G9} history={history} textsize="text-2xl"/>
+          <Line config={vars.G10} history={history} textsize="text-2xl"/>
+          <Line config={vars.G11} history={history} textsize="text-2xl"/>
 
         </div>
 
@@ -390,72 +503,12 @@ export default function Dashboard() {
     <div className={`w-full h-fit ${c2} flex flex-col items-center justify-center text-black text-3xl border-2 border-t-0 border-black`}>
 
 
-       <Line unit={1} config = {vars.T1} type={1} history={history}/>
-       <Line unit={1} config = {vars.T2} type={1} history={history}/>
-       <Line unit={1} config = {vars.T3} type={1} history={history}/>
+       <Line config = {vars.T1} history={history}  type={1} />
+       <Line config = {vars.T2} history={history} type={1}/>
+       <Line config = {vars.T3} history={history}  type={1} />
 
     </div>
 
-
-    <div
-      className="bg-white rounded-xl shadow-md p-5 flex flex-col gap-3"
-      style={{ width: 420 }}
-    >
-      <div className="text-lg font-semibold">
-        PLC Write Command
-      </div>
-
-      <div className="text-sm text-gray-500">
-        Generates <code>addressAvalueA</code> for <b>test.txt</b>
-      </div>
-
-      {/* Address input */}
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-medium">Address</label>
-        <input
-          type="number"
-          value={addrInput}
-          onChange={(e) => setAddrInput(e.target.value)}
-          className="border rounded-lg px-3 py-2 text-sm"
-          placeholder="e.g. 40001"
-        />
-      </div>
-
-      {/* Value input */}
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-medium">Value</label>
-        <input
-          type="number"
-          value={valInput}
-          onChange={(e) => setValInput(e.target.value)}
-          className="border rounded-lg px-3 py-2 text-sm"
-          placeholder="e.g. 123"
-        />
-      </div>
-
-      {/* Preview */}
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-medium">Preview</label>
-        <div className="border border-dashed rounded-lg p-2 text-sm font-mono min-h-[36px]">
-          {writePreview || "—"}
-        </div>
-      </div>
-
-      {/* Button */}
-      <button
-        onClick={handleWriteClick}
-        className="bg-[#3CCE58] text-white rounded-lg py-2 font-semibold hover:opacity-90"
-      >
-        Write test.txt
-      </button>
-
-      {/* Status */}
-      {writeStatus && (
-        <div className="text-sm font-mono">
-          {writeStatus}
-        </div>
-      )}
-    </div>
 
 
 
